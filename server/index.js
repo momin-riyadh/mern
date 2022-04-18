28ccb1c90e7ce6dae4097c9f82382103f23e97a6
momin-riyadh <mominriyadh@gmail.com>
2022-04-18 20:19:29 +0600

    clien and server folder added

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://mominriyadh:@Alkarim786@cluster0.jqiet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+// mongoose.connect(CONNECTION_URL, {useNewUrlParser: true})
+//     .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+//     .catch((error) => console.log(error.message));
+//
+// mongoose.set(useFindAndModify, false)
+
+//
+// mongoose.connect(CONNECTION_URL, {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     retryWrites: true,
+//     w: "majority",
+// });
+//
+// mongoose.Promise = global.Promise;
+
+const URI = process.env.CONNECTION_URL;
+mongoose.connect(URI, { useUnifiedTopology: true }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+} )
