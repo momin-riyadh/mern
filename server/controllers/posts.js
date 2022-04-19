ea07554ed8cf79598ccad61904b3b4a802a08f5d
momin-riyadh <mominriyadh@gmail.com>
2022-04-19 22:04:16 +0600

    fix and deploy

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,14 @@
-export const getPosts = (req, res) => {
-    res.send('THIS WORKS FOR ME');
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation');
 }
