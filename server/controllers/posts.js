92964e934fcf76b2d0193e4be48880ed5a3581e3
momin-riyadh <mominriyadh@gmail.com>
2022-04-20 20:40:08 +0600

    fix and deploy

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,6 +9,13 @@ export const getPosts = async (req, res) => {
     }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost)
+    } catch (e) {
+        res.status(409).json({message: e.message});
+    }
 }
