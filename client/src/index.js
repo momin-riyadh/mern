0d4b57f6cca315cdd6849613a546b26b65566195
momin-riyadh <mominriyadh@gmail.com>
2022-04-25 21:27:53 +0600

    fix and deploy

--- client/src/index.js
+++ client/src/index.js
@@ -7,16 +7,16 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from "redux";
 import thunk from 'redux-thunk';
 import reducers from './reducers'
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store ={store}>
+    <Provider store={store}>
         <React.StrictMode>
             <App/>
         </React.StrictMode>
     </Provider>
-
 );
 
 // If you want to start measuring performance in your app, pass a function
