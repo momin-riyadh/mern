b4a2103a3b10b7c4172f07960d04356a5b57f9c8
Momin Riyadh <mominriyadh@gmail.com>
2022-04-28 11:35:19 +0600

    fix and deploy

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,57 @@
 import React from "react";
+import {Card, CardActions, CardContent, CardMedia, Button, Typography} from "@material-ui/core";
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcons from '@material-ui/icons/MoreHoriz';
+import moment from "moment";
 import useStyles from './styles';
 
-const Post = () => {
+const Post = ({post}) => {
     const classes = useStyles()
     return (
-        <h1>Post</h1>
+        <Card className={classes.card}>
+
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+
+            <div className={classes.overlay}>
+                <Typography variant="h6">
+                    {post.creator}
+                </Typography>
+                <Typography varient={"body2"}>
+                    {moment(post.createdAt).formNow()}
+                </Typography>
+            </div>
+
+            <div className={classes.overlay2}>
+                <Button style={{color: 'white'}} size="small" onClick={() => {
+                }}>
+                    <MoreHorizIcons fontSize="default"/>
+                </Button>
+            </div>
+
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">
+                    {post.tags.map((tag) => `#${tag}`)}
+                </Typography>
+            </div>
+
+            <CardContent>
+                <Typography className={classes.title} variant="h5" gutterBottom>
+                    {post.message}
+                </Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                    <Button size="small" color="primary" onClick={() =>{}}>
+                        <ThumbUpAltIcon fontSize="small"/>
+                        Like
+                        {post.likeCount}
+                    </Button>
+                <Button size="small" color="primary" onClick={() =>{}}>
+                        <DeleteIcon fontSize="small"/>
+                        Delete
+                    </Button>
+            </CardActions>
+        </Card>
     )
 }
 
