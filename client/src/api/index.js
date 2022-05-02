45e15f5fb8023f68609f2b1c610a6625a66ba4d5
momin-riyadh <mominriyadh@gmail.com>
2022-05-02 23:44:14 +0600

    fix and deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'https://newmern-project.herokuapp.com/posts';
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ dotenv.config();
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
-app.use('/posts', postRoutes)
+app.use('/posts', postRoutes);
+app.get('/', (req, res)=>{
+    res.send('Hello to Memories API')
+})
 
 // const CONNECTION_URL = "mongodb+srv://mominriyadh:@Alkarim786@cluster0.jqiet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
