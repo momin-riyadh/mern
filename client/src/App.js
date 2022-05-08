aafab7293b907f41d2f036dca426ce87de2c4643
Momin Riyadh <mominriyadh@gmail.com>
2022-05-08 17:36:18 +0600

    fix and deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
 import './App.css';
-import {Container, Grow, Grid} from '@material-ui/core';
+import {Container} from '@material-ui/core';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth"
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Container maxwidth={"lg"}>
-                <Navbar/>
-                <Switch>
-                    
-                </Switch>
-                <Home/>
-            </Container>
-        </BrowserRouter>
+const App = () => (
+    <BrowserRouter>
+        <Container maxwidth={"lg"}>
+            <Navbar/>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/auth" exact component={Auth}/>
+            </Switch>
+        </Container>
+    </BrowserRouter>
+);
 
-    );
-}
 
 export default App;
