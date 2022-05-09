6f0d1e3893c877887985ac529c31474ab2c4a64c
Momin Riyadh <mominriyadh@gmail.com>
2022-05-09 09:42:27 +0600

    fix and deploy

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {useState,} from 'react';
+import {Avatar, Button, Paper, Grid, Typography, Container, TextField} from "@material-ui/core";
+import useStyles from './styles';
+import {LockOpenOutlined} from "@material-ui/icons";
+import Input from './Input';
 
-const Auth = ()=>{
-    const state = null;
-    return(
-            <div>
-                AUTH
-            </div>
+const Auth = () => {
+    const classes = useStyles();
+    const [showPassword, setShowPassword] = useState(false)
+    const isSignup = false;
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
+    return (
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevatiuon={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOpenOutlined/>
+                </Avatar>
+                <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                                <>
+                                        <Input name="firstName" label="First Name" handlechange={handleChange}
+                                               autoFocus half></Input>
+                                        <Input name="firstName" label="First Name" handlechange={handleChange}
+                                               autoFocus half></Input>
+                                </>
+                            )
+                        }
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email"></Input>
+                        <Input name="password" label="Password" handleChange={handleChange} type="password"></Input>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
     )
 }
 
