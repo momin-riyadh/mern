924af01bb8908f291e7ca565525002cb320a8382
Momin Riyadh <mominriyadh@gmail.com>
2022-05-09 10:52:22 +0600

    fix and deploy

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -6,8 +6,12 @@ import Input from './Input';
 
 const Auth = () => {
     const classes = useStyles();
-    const [showPassword, setShowPassword] = useState(false)
-    const isSignup = false;
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
+
+    const handleShowPassword = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword)
+    };
 
     const handleSubmit = () => {
 
@@ -16,6 +20,11 @@ const Auth = () => {
     const handleChange = () => {
 
     };
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignUp) => !prevIsSignUp);
+        handleShowPassword(false)
+    }
     return (
         <Container component="main" maxWidth="xs">
             <Paper className={classes.paper} elevatiuon={3}>
@@ -28,15 +37,28 @@ const Auth = () => {
                         {
                             isSignup && (
                                 <>
-                                        <Input name="firstName" label="First Name" handlechange={handleChange}
-                                               autoFocus half></Input>
-                                        <Input name="firstName" label="First Name" handlechange={handleChange}
-                                               autoFocus half></Input>
+                                    <Input name="firstName" label="First Name" handlechange={handleChange}
+                                           autoFocus half></Input>
+                                    <Input name="lastName" label="Last Name" handlechange={handleChange}
+                                           autoFocus half></Input>
                                 </>
                             )
                         }
                         <Input name="email" label="Email Address" handleChange={handleChange} type="email"></Input>
-                        <Input name="password" label="Password" handleChange={handleChange} type="password"></Input>
+                        <Input name="password" label="Password" handleChange={handleChange}
+                               type={showPassword ? "text" : "password"}
+                               handleShowPassword={handleShowPassword}></Input>
+                        {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange}
+                                            type="password"></Input>}
+                    </Grid>
+                    <Button type="submit" fullWidth variant={"contained"} color={"primary"} className={classes.submit}>
+                        {isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+
+                    <Grid item className={classes.footerText}>
+                        <small onClick={switchMode}>
+                            {isSignup ? 'Already have an account ? Sign In' : "Don't have an account ? Sign Up"}
+                        </small>
                     </Grid>
                 </form>
             </Paper>
